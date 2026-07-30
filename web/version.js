45e4683fe125d2f4400f772eb6a98e9312cbd42b
PascalHuang0611 <pascal.h@trevi.cc>
2026-07-30 16:36:55 +0800

    v1.11

--- web/version.js
+++ web/version.js
@@ -1,5 +1,5 @@
 // 全站共用版本號,升版只改這裡
-const APP_VERSION = '1.10';
+const APP_VERSION = '1.11';
 
 document.addEventListener('DOMContentLoaded', () => {
     const versionTag = document.createElement('div');
